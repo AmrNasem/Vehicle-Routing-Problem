6d68e04b57b61788a2492bafca19056493c04ef6
Amr <amr.nasem51@gmail.com>
2023-04-03 00:12:58 +0200

    Change the depot + Code Refactoring

--- Location.js
+++ Location.js
@@ -1,12 +1,14 @@
 class Location {
-  constructor(id, x, y, depot = false) {
-    this.id = id.toString();
+  constructor(x, y, depot = false) {
+    this.id = Math.random().toString();
     this.x = x;
     this.y = y;
     this.depot = depot;
+    this.domElement = null;
   }
 
   createLocation(ctx) {
+    // Canvas Location
     let radius = this.depot ? 7 : 5;
 
     ctx.beginPath();
@@ -14,11 +16,14 @@ class Location {
     ctx.arc(this.x * 20, this.y * 20, radius, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
-  }
 
-  createLocationElement() {
+    // DOM Location
     const location = document.createElement("div");
     location.className = "location";
+    this.depot
+      ? location.classList.add("depot")
+      : location.classList.remove("depot");
+
     location.setAttribute("data-x", this.x);
     location.setAttribute("data-y", this.y);
     location.setAttribute("data-id", this.id);
@@ -26,10 +31,15 @@ class Location {
     const li = document.createElement("li");
     li.innerHTML = `(${this.x}, ${this.y})`;
 
+    const span = document.createElement("span");
+    if (this.depot) span.innerHTML = "Depot";
+
     const button = document.createElement("button");
     button.innerHTML = "Delete";
 
-    location.append(li, button);
+    location.append(li, span, button);
+    this.domElement = location;
+
     return location;
   }
 }
--- main.js
+++ main.js
@@ -4,6 +4,23 @@ const ctx = myCanvas.getContext("2d");
 const height = myCanvas.height;
 const width = myCanvas.width;
 
+// ====================== Reset ================================
+const reset = () => {
+  ctx.clearRect(0, 0, width, height);
+
+  locationElements.replaceChildren("");
+  locations.forEach((location) => {
+    // Set another depot instead of the removed one
+    if (init) {
+      location.depot = true;
+      init = false;
+    }
+    locationElements.prepend(location.createLocation(ctx));
+    location.domElement.addEventListener("click", setDepot);
+    location.domElement.addEventListener("click", removeLocation);
+  });
+};
+
 // DOM Variables
 const form = document.querySelector(".container section form");
 const x = document.getElementById("x-coordinate");
@@ -11,59 +28,64 @@ const y = document.getElementById("y-coordinate");
 const locationElements = document.querySelector(
   ".container section .locations"
 );
+
 let locations = [];
+
+// Set the depot
+const setDepot = (e) => {
+  if (e.target.classList.contains("location")) {
+    locations.forEach((location) => (location.depot = false));
+
+    locations.find(
+      (location) => location.id === e.target.dataset.id
+    ).depot = true;
+    reset();
+  }
+};
 
 // Add Location
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  if (x.value.trim() === "" || y.value.trim() === "") return;
+  if (x.value === "" || y.value === "") return;
 
   for (let i = 0; i < locations.length; i++) {
     if (locations[i].x === x.value && locations[i].y === y.value) return;
   }
-  const location = new Location(Math.random(), x.value, y.value);
+  const location = new Location(x.value, y.value);
   locations.push(location);
-  location.createLocation();
-  locationElements.prepend(location.createLocationElement());
+  location.createLocation(ctx);
+  reset();
+
   x.value = y.value = "";
   x.focus();
 });
 
 // Remove Location
-locationElements.addEventListener("click", (e) => {
-  if (e.target.tagName !== "BUTTON") return;
-  locations = locations.filter(
-    (location) => location.id !== e.target.parentElement.dataset.id
-  );
-
-  draw();
-  e.target.parentElement.remove();
-});
+const removeLocation = (e) => {
+  if (e.target.tagName === "BUTTON") {
+    locations = locations.filter((location) => {
+      if (location.id !== e.target.parentElement.dataset.id) {
+        return true;
+      }
+      if (location.depot) init = true;
+      return false;
+    });
+    reset();
+  }
+};
 
 // Generate Locations
 let init = true;
 for (let i = 0; i < 15; i++) {
   const location = new Location(
-    i,
     Math.floor(Math.random() * 40),
     Math.floor(Math.random() * 20),
     init
   );
   init = false;
-  locations.push(location);
   location.createLocation(ctx);
-  locationElements.prepend(location.createLocationElement());
+  locations.push(location);
 }
-// ====================== Graph ================================
 
-const draw = () => {
-  ctx.clearRect(0, 0, width, height);
-
-  locationElements.replaceChildren("");
-  locations.map((location) => {
-    location.createLocation(ctx);
-    locationElements.prepend(location.createLocationElement());
-  });
-};
-
-draw();
+// Reset Canvas
+reset();
